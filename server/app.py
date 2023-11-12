4387360dedfa7d8c21f4642a8fc43e858c60bfdb
Donghee Park <dongheepark@gmail.com>
2023-11-13 08:01:37 +0900

    ros launch 파일과 연결, 결과 페이지 업데이트

--- server/app.py
+++ server/app.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template, request
 import os
-#  from uwsgidecorators import thread
+import requests
 
 app = Flask(__name__)
 
-FIFO_TX_FILE = "/tmp/pipe_tx"
-FIFO_RX_FILE = "/tmp/pipe_rx"
+FIFO_TX_FILE = "/tmp/wde_ros_tx"
+FIFO_RX_FILE = "/tmp/wde_ros_rx"
 
 def init_fifo():
     if not os.path.exists(FIFO_TX_FILE):
@@ -13,62 +13,77 @@ def init_fifo():
     if not os.path.exists(FIFO_RX_FILE):
         os.mkfifo(FIFO_RX_FILE)
 
-#  @thread
-#  def uwsgi_task(duration):
-#      for i in range(duration):
-#          print("Working in uwsgi thread... {}/{}".format(i + 1, duration))
-#          time.sleep(1)
-#
+
 def listen_ros_command(duration):
     if not os.path.exists(FIFO_RX_FILE):
         return
 
     while True:
-        with open(FIFO_RX_FILE, 'r') as fifo:
+        with open(FIFO_RX_FILE, "r") as fifo:
             line = fifo.readline()
             print(line)
-            if line.startswith('finish'):
-                print('finish ros simulation')
-   
-@app.route('/')
+            if line.startswith("finish"):
+                print("finish ros simulation")
+
+
+@app.route("/")
 def index():
-    return render_template('index.html')
+    subject = 'home'
+    return render_template('index.html', subject=subject)
 
-@app.route('/new')
+@app.route("/new", methods=["GET"])
 def new_simulation():
-    code0 = ''
-    code1 = ''
-    code2 = ''
+    code0 = ""
+    code1 = ""
+    code2 = ""
     current_path = os.path.dirname(os.path.abspath(__file__))
-    with open(current_path + '/control_node_1.py', 'r') as f:
+    with open(current_path + "/control_node_1.py", "r") as f:
         code0 = f.read()
-    with open(current_path + '/control_node_2.py', 'r') as f:
+    with open(current_path + "/control_node_2.py", "r") as f:
         code1 = f.read()
-    with open(current_path + '/control_node_3.py', 'r') as f:
+    with open(current_path + "/control_node_3.py", "r") as f:
         code2 = f.read()
+        subject = 'new'
+    return render_template("new.html", code0=code0, code1=code1, code2=code2)
 
-    return render_template('new.html', code0=code0, code1=code1, code2=code2)
-
-@app.route('/evaluate', methods=['POST'])
+@app.route("/evaluate", methods=["POST"])
 def evaluate_device():
-    #  print(request.form)
-    device = request.form['device']
-    body = request.form['body']
-    task = request.form['task']
+    print(request.form)
+    device = request.form["device"]
+    body = request.form["body"]
+    task = request.form["task"]
+    code = request.form["code"]
     # TODO: change me
-    joint = 'joint1'
-    url = f'/?device={device}&body={body}&joint={joint}'
-    editor = request.form['editor']
-    with open('/tmp/new_code.py', 'w') as f:
+    joint = "joint1"
+    url = f"?device={device}&body={body}&joint={joint}&task={task}&code=code{code}"
+    editor = request.form["editor"]
+    with open("/tmp/new_code.py", "w") as f:
         f.write(editor)
 
     # start ros launch
-    with open(FIFO_TX_FILE, 'w') as f:
-        f.write('start')
-        print('start ros simulation')
+    with open(FIFO_TX_FILE, "w") as f:
+        f.write("start")
+        print("start simulation")
 
-    return render_template('result.html', url=url)
+    #  ros launch
+    with open(FIFO_RX_FILE, "r") as f:
+        line = f.readline()
+        if line.startswith("finish"):
+            print("finish simulation")
+        else:
+            print("error simulation")
 
-if __name__ == '__main__':
+    r = requests.get(f"http://localhost/usability/score{url}")
+    usability = r.text
+    r = requests.get(f"http://localhost/wearability/score{url}")
+    wearability = r.text
+    r = requests.get(f"http://localhost/interactivity/score{url}")
+    interactivity = r.text
+    totalscore = (float(usability) + float(wearability) + float(interactivity)) / 3
+
+    return render_template("result.html", url=url, device=device, body=body, task=task, code=f"code{code}", totalscore=totalscore, usability=usability, wearability=wearability, interactivity=interactivity)
+
+
+if __name__ == "__main__":
     init_fifo()
-    app.run(host='0.0.0.0', port=5054, debug=True)
+    app.run(host="0.0.0.0", port=5054, debug=True)
