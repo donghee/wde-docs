784472c75d52ac33cc63c5a86b6e74aa7e5cbd59
Donghee Park <dongheepark@gmail.com>
2023-11-14 15:21:25 +0900

    Update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./server/templates/*.html", "./server/templates/layouts/*.html", "../wde-wearability/web/templates/*.html", "../wde-interativity/web/templates/*.html", "../wde-usability/web/templates/*.html" ],
+  content: ["./server/templates/*.html", "./server/templates/layouts/*.html", "../wde-wearability/web/templates/*.html", "../wde-interativity/web/templates/*.html", "../wde-usability/web/templates/*.html", "../wde-visualization/web/templates/*.html" ],
   theme: {
     extend: {},
   },
